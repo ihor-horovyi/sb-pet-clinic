b3aff936b0f000e9f4f22d3f32ad26e1968f9294
ihor.horovyi <ihor.horovyi@vitech.com.ua>
2019-01-20 20:06:49 +0200

    Init project

---
+++ src/main/java/horovyi/petclinic/SbPetClinicApplication.java
@@ -0,0 +1,14 @@
+package horovyi.petclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SbPetClinicApplication.class, args);
+    }
+
+}
+
---
+++ src/test/java/horovyi/petclinic/SbPetClinicApplicationTests.java
@@ -0,0 +1,17 @@
+package horovyi.petclinic;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SbPetClinicApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
